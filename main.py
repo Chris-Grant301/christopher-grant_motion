b7dc08947cbc0b289196d6c087634f84429c6156
Chris-Grant301 <72807122+Chris-Grant301@users.noreply.github.com>
2021-01-27 11:02:34 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-input.rotation(Rotation.PITCH)
+while True:
+    print("Rotation Pitch is: " + input.rotation(Rotation.PITCH))
+    if input.rotation(Rotation.PITCH) <45:
+        light.set_all(light.rgb(255,165,0))
+    else:
+        light.clear()
